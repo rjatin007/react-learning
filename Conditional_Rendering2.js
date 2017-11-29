b221f3d08e4b756b6eb7efe5101d880c3ad5e07e
rjatin007 <30732342+rjatin007@users.noreply.github.com>
2017-11-29 23:20:14 +0530

    Create Conditional_Rendering2.js

---
+++ Conditional_Rendering2.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+state={
+  persons:[
+    { name : 'Jatin' , age : 26 },
+    { name : 'Riship' , age : 26 },
+    { name : 'Amit' , age : 28 }
+  ],
+  showPersons:true
+}
+
+onToggle=()=>{
+  const show=this.state.showPersons;
+  this.setState({
+    showPersons : !show
+  });
+}
+  render() {
+    const { persons, showPersons } = this.state;
+    let showCase = null;
+    if(this.state.showPersons){
+     showCase  =  (
+          persons.map(person=>(
+            <Person name={person.name} age = {person.age}/>
+          )
+        )
+      )
+    }
+    return (
+      <div className="App">
+      {showCase}
+      <button onClick={this.onToggle}> Render Conditinally </button> 
+      </div>
+    );
+  }
+}
+
+export default App;
